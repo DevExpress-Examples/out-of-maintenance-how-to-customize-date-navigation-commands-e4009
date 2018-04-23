ad5e84b00f42af5a083e32e0c95a9d94fffb5ee7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DevExpress.Web.ASPxScheduler;
+using DevExpress.Web.ASPxScheduler.Internal;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+
+    }
+
+    protected void ASPxScheduler1_BeforeExecuteCallbackCommand(object sender, DevExpress.Web.ASPxScheduler.SchedulerCallbackCommandEventArgs e) {
+        if (e.CommandId == SchedulerCallbackCommandId.NavigateForward) {
+            e.Command = new CustomNavigateForwardCallbackCommand((ASPxScheduler)sender);
+        }
+        else if (e.CommandId == SchedulerCallbackCommandId.NavigateBackward) {
+            e.Command = new CustomNavigateBackwardCallbackCommand((ASPxScheduler)sender);
+        }
+    }
+}
+
+public class CustomNavigateForwardCallbackCommand : NavigateForwardCallbackCommand {
+    public CustomNavigateForwardCallbackCommand(ASPxScheduler control)
+        : base(control) {
+    
+    }
+    
+    protected override void ExecuteCore() {
+        this.Control.Start = this.Control.ActiveView.GetVisibleIntervals().End;
+    }
+}
+
+public class CustomNavigateBackwardCallbackCommand : NavigateBackwardCallbackCommand {
+    public CustomNavigateBackwardCallbackCommand(ASPxScheduler control)
+        : base(control) {
+
+    }
+
+    protected override void ExecuteCore() {
+        TimeSpan duration = this.Control.ActiveView.GetVisibleIntervals().Duration;
+        this.Control.Start = this.Control.ActiveView.GetVisibleIntervals().Start.AddHours(-duration.TotalHours);
+    }
+}
